fcec18e14e16dd69c20fc4a3a09dd90006675fed
Sujith <sujith.sappani@gmail.com>
2025-04-02 22:07:35 +0530

    Day 3

---
+++ src/Leet_2.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+
+public class Leet_2 {
+
+    public class ListNode {
+
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int l = 0;
+
+        while () {
+
+        }
+        return 
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> l1 = new LinkedList<>(Arrays.asList(1, 2, 3));
+        LinkedList<Integer> l2 = new LinkedList<>(Arrays.asList(1, 2, 3));
+        System.out.println(addTwoNumbers(l1, l2));
+
+    }
+}
