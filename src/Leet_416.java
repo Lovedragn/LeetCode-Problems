c379d0a88e056e372c58b8c2122f8a6b82c7d66c
Sujith <sujith.sappani@gmail.com>
2025-04-08 23:03:20 +0530

    Day-128

--- src/Leet_416.java
+++ src/Leet_416.java
@@ -2,13 +2,28 @@
 public class Leet_416 {
 
     public static boolean canPartition(int[] nums) {
-        boolean res = true;
-        
-        return res;
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        if (totalSum % 2 != 0) return false;
+
+        int target = totalSum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true; 
+
+        for (int num : nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+
+        return dp[target];
     }
 
-    public static void main(String args[]) {
-        int arr[] = {1, 5, 11, 5};
-        System.out.println(canPartition(arr));
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        System.out.println(canPartition(arr)); // Expected output: true
     }
 }
