4c9154a10f30d2415743b60865a886c14040b131
Sujith <sujith.sappani@gmail.com>
2025-05-19 20:41:35 +0530

    Day-144

---
+++ src/Leet_24.java
@@ -0,0 +1,57 @@
+
+
+public class Leet_24 {
+
+    class Node {
+
+        int data;
+        Node next;
+    }
+
+    Node head = null;
+
+    public void insert(int data) {
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+
+        if (head == null) {
+            head = node;
+        } else {
+            Node n = head;
+            while (n.next != null) {
+                n = n.next;
+            }
+            n.next = node;
+        }
+    }
+
+    public void show() {
+        Node n = head;
+        while (n != null) {
+            System.out.print(n.data);
+            if (n.next != null) {
+                System.out.print(" -> ");
+            }
+            n = n.next;
+        }
+    }
+
+    public Node swapPairs(Node head) {
+        
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Leet_24 LL = new Leet_24();
+        //call methods
+        LL.insert(5);
+        LL.insert(2);
+        LL.insert(13);
+        LL.insert(3);
+        LL.insert(8);
+        //call
+        LL.head = LL.swapPairs(LL.head);
+        LL.show();
+    }
+}
