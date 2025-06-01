11ccef960689b804c99eecf6a35aa0fe14969f95
Sujith <sujith.sappani@gmail.com>
2025-06-01 23:23:09 +0530

    Day-148

--- src/Leet_617.java
+++ src/Leet_617.java
@@ -58,8 +58,45 @@ public class Leet_617 {
         }
         Inorder_temp(root.left, list);
         list.add(root.val);
-        Inorder_temp(root.right , list);
-        
+        Inorder_temp(root.right, list);
+
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Preorder_temp(root, list);
+        return list;
+    }
+
+    public static void Preorder_temp(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        Preorder_temp(root.left, list);
+        Preorder_temp(root.right, list);
+    }
+
+    public static List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Postorder_temp(root, list);
+        return list;
+    }
+
+    public static void Postorder_temp(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        if (root.right == null && root.left == null) {
+            list.add(root.val);
+            return;
+        }
+        Postorder_temp(root.left, list);
+        if (root.left == null || root.right != null) {
+            Postorder_temp(root.right, list);
+        }
+
+        list.add(root.val);
 
     }
 
@@ -68,14 +105,14 @@ public class Leet_617 {
         root1.left = new TreeNode(2);
         root1.left.right = new TreeNode(3);
         root1.left.right.right = new TreeNode(4);
-        root1.left.right.right.right = new TreeNode(5);
+        root1.right = new TreeNode(5);
 
         // TreeNode root2 = new TreeNode(2);
         // root2.left = new TreeNode(1);
         // root2.right = new TreeNode(3);
         // root2.left.right = new TreeNode(4);
         // root2.right.right = new TreeNode(7);
-        System.out.println("Merged Root Value: " + Inorder(root1));
+        System.out.println("Merged Root Value: " + postorderTraversal(root1));
     }
 
 }
