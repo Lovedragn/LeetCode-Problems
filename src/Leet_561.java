30c9afbccf85cf7c9dacec7e3638562c8d9d0a52
Dev-sujithsappani <sujith.sappani.works@gmail.com>
2026-01-08 22:42:49 +0530

    Day-195

---
+++ src/Leet_561.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Leet_561 {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            result += nums[i];
+        }
+        return result;
+    }
+}
