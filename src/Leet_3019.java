5d2994a9373298f8f0a8c2cfcf282b5f2b216f2a
Dev-sujithsappani <sujith.sappani.works@gmail.com>
2026-01-10 23:32:04 +0530

    Day-198

--- src/Leet_3019.java
+++ src/Leet_3019.java
@@ -1,3 +1,17 @@
 public class Leet_3019 {
-    
+    public static int countKeyChanges(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) != s.charAt(i + 1) && s.charAt(i) != Character.toLowerCase(s.charAt(i + 1))
+                    && s.charAt(i) != Character.toUpperCase(s.charAt(i + 1))) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "aABbCc";
+        System.out.println(countKeyChanges(s)); // Expected output: 2
+    }
 }
