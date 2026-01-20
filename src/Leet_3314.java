6c2965d60dfe75b262a3633d24470aafe6440f88
Dev-sujithsappani <sujith.sappani.works@gmail.com>
2026-01-20 10:42:49 +0530

    Day-206

---
+++ src/Leet_3314.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Leet_3314 {
+    public static int[] minBitwiseArray(List<Integer> nums) {
+        int[] res = new int[nums.size()];
+        for (int i = 0; i < nums.size(); i++) {
+            int n = nums.get(i);
+            if ((n & 1) == 1) {
+                res[i] = n & ~(((n + 1) & ~n) >> 1);
+            } else {
+                res[i] = -1;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String args[]) {
+        List<Integer> arr = new ArrayList<>(Arrays.asList(2, 3, 5, 7));
+        int res[] = minBitwiseArray(arr);
+        for (int i : res) {
+            System.out.println(i);
+        }
+    }
+}
