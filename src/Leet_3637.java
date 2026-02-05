52b99a04bf0db7552b49765c0b9b398fee061816
Dev-sujithsappani <sujith.sappani.works@gmail.com>
2026-02-05 23:21:37 +0530

    Day-215

--- src/Leet_3637.java
+++ src/Leet_3637.java
@@ -1,3 +1,26 @@
 public class Leet_3637 {
-    
+
+    public static boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        int i = 1;
+        while (i < n && nums[i - 1] < nums[i]) {
+            i++;
+        }
+        int p = i - 1;
+        while (i < n && nums[i - 1] > nums[i]) {
+            i++;
+        }
+        int q = i - 1;
+        while (i < n && nums[i - 1] < nums[i]) {
+            i++;
+        }
+        int flag = i - 1;
+        return (p != 0) && (q != p) && (flag == n - 1 && flag != q);
+
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 2, 1, 3 };
+        System.out.println(isTrionic(arr) ? "T" : "F");
+    }
 }
