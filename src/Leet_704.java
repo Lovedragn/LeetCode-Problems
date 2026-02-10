84e74ff87f508b9a058755eaa0f3a7a26bee0089
Dev-sujithsappani <sujith.sappani.works@gmail.com>
2026-02-10 18:58:33 +0530

    Day-219

--- src/Leet_704.java
+++ src/Leet_704.java
@@ -1,16 +1,16 @@
 public class Leet_704 {
     public static int search(int[] nums, int target) {
-        int res =-1;
-        int l =0,r = nums.length-1;
-        while(l <=r){
-            int mid = l+(r-l)/2;
-            if(nums[mid] == target){
+        int res = -1;
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
                 res = mid;
                 break;
-            }else if(nums[mid] < target){
-                l = mid+1;
-            }else if(nums[mid] > target){
-                r = mid-1;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
             }
         }
         return res;
