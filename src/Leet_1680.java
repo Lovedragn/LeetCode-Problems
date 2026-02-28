f6def72efed3bf11e6261571a93316fb26f9ccd7
Dev-sujithsappani <sujith.sappani.works@gmail.com>
2026-02-28 12:16:09 +0530

    Day-227

--- src/Leet_1680.java
+++ src/Leet_1680.java
@@ -1,13 +1,21 @@
+import java.math.BigInteger;
+
 public class Leet_1680 {
     public static int concatenatedBinary(int n) {
-        StringBuilder str = new StringBuilder();
-        for (int i = 1; i <= n; i++) {
-            String value = Integer.toBinaryString(i);
-            str.append(value);
+        final int MOD = 1000000007;
+        long result = 0;
+        int length = 0;
 
+        for (int i = 1; i <= n; i++) {
+            // Increase length when i is a power of 2
+            if ((i & (i - 1)) == 0) {
+                length++;
+            }
+            // Shift result left by length bits and add i
+            result = ((result << length) % MOD + i) % MOD;
         }
-        Long res = ((Long.parseUnsignedLong(str.toString(), 2) % ((int) Math.pow(10, 9)) + 7));
-        return Integer.parseInt(res.toString());
+
+        return (int) result;
     }
 
     public static void main(String args[]) {
