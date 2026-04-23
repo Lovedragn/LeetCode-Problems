d54a2a9489151b3841694354762169f4c2976d6a
Dev-sujithsappani <sujith.sappani.works@gmail.com>
2026-04-23 12:40:22 +0530

    Day-250

--- src/Leet_2615.java
+++ src/Leet_2615.java
@@ -6,31 +6,36 @@ public class Leet_2615 {
     public static long[] distance(int[] nums) {
         long[] res = new long[nums.length];
         HashMap<Integer, List<Integer>> map = new HashMap<>();
-
+        
         for (int i = 0; i < res.length; i++) {
             if (!map.containsKey(nums[i])) {
                 map.put(nums[i], new ArrayList<>());
             }
-            map.put(nums[i], map.get(nums[i])).add(i);
+            map.get(nums[i]).add(i);
         }
 
-        for (int i = 0; i < res.length; i++) {
-            if (res[i] != 0) {
+        for (List<Integer> temp : map.values()) {
+            if (temp.size() <= 1)
                 continue;
-            }            
-            List<Integer> temp = new ArrayList<>(map.get(nums[i]));
-             temp.remove(Integer.valueOf(i));
-            int count = 0;
-            for (int j = 0; j < temp.size(); j++) {
-                count += Math.abs(i -   temp.get(j));
+
+            long[] prefix = new long[temp.size() + 1];
+            for (int k = 0; k < temp.size(); k++) {
+                prefix[k + 1] = prefix[k] + temp.get(k);
             }
-            res[i] = count;
+
+            for (int k = 0; k < temp.size(); k++) {
+                int idx = temp.get(k);
+                long left = (long) idx * k - prefix[k];
+                long right = (prefix[temp.size()] - prefix[k + 1]) - (long) idx * (temp.size() - k - 1);
+                res[idx] = left + right;
+            }
         }
+
         return res;
     }
 
     public static void main(String[] args) {
-        int[] nums = { 1, 3, 1, 1, 2 };
+        int[] nums = {1,3,1,1,2 };
         long[] res = distance(nums);
 
         for (long l : res) {
