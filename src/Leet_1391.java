bd4b5a7a80c1a2138b272982c560d9de769f51ce
Dev-sujithsappani <sujith.sappani.works@gmail.com>
2026-04-27 23:21:09 +0530

    Day-252

--- src/Leet_1391.java
+++ src/Leet_1391.java
@@ -2,55 +2,105 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Leet_1391 {
+
     public static HashMap<Integer, List<Character>> range;
     public static boolean[][] visited;
 
     public static boolean hasValidPath(int[][] grid) {
+
         range = new HashMap<>();
-        range.put(1, List.of('l', 'r', 'l'));
-        range.put(2, List.of('t', 'd', 't'));
-        range.put(3, List.of('l', 'd', 't'));
-        range.put(4, List.of('r', 'd', 't'));
-        range.put(5, List.of('t', 'l', 'r'));
-        range.put(6, List.of('t', 'r', 'l'));
+
+        // [enterFrom , exit1 , exit2]
+        range.put(1, List.of('l', 'r', 'l')); // left-right
+        range.put(2, List.of('t', 'd', 't')); // top-down
+        range.put(3, List.of('l', 'd', 't')); // left-top
+        range.put(4, List.of('r', 'd', 't')); // right-top
+        range.put(5, List.of('t', 'l', 'r')); // top-left
+        range.put(6, List.of('t', 'r', 'l')); // top-right
 
         visited = new boolean[grid.length][grid[0].length];
 
-        boolean res = router(grid, 0, 0, range.get(grid[0][0]).getLast());
-        return res;
+        return router(grid, 0, 0, 's');
+    }
+
+    public static boolean isValid(char prev, int type) {
+
+        List<Character> temp = range.get(type);
+
+        return temp.contains(prev);
     }
 
     public static boolean router(int[][] grid, int i, int j, char prev) {
-        if (i == grid.length - 1 && j == grid[0].length - 1) {
-            return true;
+
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) {
+            return false;
         }
+
         if (visited[i][j]) {
             return false;
         }
+
+        // check whether current block accepts previous direction
+        if (prev != 's' && !isValid(prev, grid[i][j])) {
+            return false;
+        }
+
+        if (i == grid.length - 1 && j == grid[0].length - 1) {
+            return true;
+        }
+
         visited[i][j] = true;
 
-        boolean T = false, D=false, L=false, R=false;
-        // top
-        if (i - 1 >= 0 && i - 1 < grid.length && range.get(grid[i - 1][j]).getFirst().equals(prev)) {
-            T = router(grid, i - 1, j, range.get(grid[i - 1][j]).getLast());
+        int type = grid[i][j];
+
+        boolean T = false, D = false, L = false, R = false;
+
+        // type 1 -> left right
+        if (type == 1) {
+            L = router(grid, i, j - 1, 'r');
+            R = router(grid, i, j + 1, 'l');
         }
-        // Down
-        if (i + 1 >= 0 && i + 1 < grid.length && range.get(grid[i + 1][j]).getFirst().equals(prev)) {
-            D = router(grid, i + 1, j, range.get(grid[i + 1][j]).getLast());
+
+        // type 2 -> top down
+        else if (type == 2) {
+            T = router(grid, i - 1, j, 'd');
+            D = router(grid, i + 1, j, 't');
         }
-        // Left
-        if (j -1 >= 0 && j < grid[0].length && range.get(grid[i][j - 1]).getFirst().equals(prev)) {
-            L = router(grid, i, j - 1, range.get(grid[i][j - 1]).getLast());
+
+        // type 3 -> left down
+        else if (type == 3) {
+            L = router(grid, i, j - 1, 'r');
+            D = router(grid, i + 1, j, 't');
         }
-        // Right
-        if (j + 1 >= 0 && j + 1 < grid[0].length && range.get(grid[i][j+1]).getFirst().equals(prev)) {
-            R = router(grid, i, j + 1, range.get(grid[i][j + 1]).getLast());
+
+        // type 4 -> right down
+        else if (type == 4) {
+            R = router(grid, i, j + 1, 'l');
+            D = router(grid, i + 1, j, 't');
         }
+
+        // type 5 -> left top
+        else if (type == 5) {
+            L = router(grid, i, j - 1, 'r');
+            T = router(grid, i - 1, j, 'd');
+        }
+
+        // type 6 -> right top
+        else if (type == 6) {
+            R = router(grid, i, j + 1, 'l');
+            T = router(grid, i - 1, j, 'd');
+        }
+
         return (T || D || L || R);
     }
 
     public static void main(String[] args) {
-        int grid[][] = { { 2, 4, 3 }, { 6, 5, 2 } };
-        System.out.println(hasValidPath(grid) ? "T" : "F ");
+
+        int grid[][] = {
+                {2, 4, 3},
+                {6, 5, 2}
+        };
+
+        System.out.println(hasValidPath(grid) ? "T" : "F");
     }
 }
